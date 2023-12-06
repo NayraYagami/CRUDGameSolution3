a515a471b0e306ae17eeac4a066c2744ee032c4f
NayraYagami <nayra.nsppp@gmail.com>
2023-12-06 14:11:40 -0300

    ajustes

--- CRUDGame/RPGModel.Context.cs
+++ CRUDGame/RPGModel.Context.cs
@@ -25,12 +25,5 @@ namespace CRUDGame
             throw new UnintentionalCodeFirstException();
         }
     
-        public virtual DbSet<Atributo> Atributoes { get; set; }
-        public virtual DbSet<Classe> Classes { get; set; }
-        public virtual DbSet<Cor> Cors { get; set; }
-        public virtual DbSet<Habilidade> Habilidades { get; set; }
-        public virtual DbSet<Personagem> Personagems { get; set; }
-        public virtual DbSet<Raca> Racas { get; set; }
-        public virtual DbSet<Subclasse> Subclasses { get; set; }
     }
 }
